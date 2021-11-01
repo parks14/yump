44810a5473e82e020b518e524a39e6ecc8c9a8da
spark <spark11491@gmail.com>
2021-11-01 00:00:21 -0400

    finish route util

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Route } from "react-router";
+import { Route, Switch } from "react-router";
 import NavbarContainer from "./navbar/navbar_container";
 import LogInFormContainer from "./session_form/login_form_container";
 import SignUpFormContainer from "./session_form/signup_form_container";
 import { Link } from "react-router-dom";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
 const App = () => (
     <div>
@@ -14,8 +15,10 @@ const App = () => (
             <NavbarContainer /> 
         </header>
 
-        <Route exact path="/login" component={LogInFormContainer} />
-        <Route excat path="/signup" component={SignUpFormContainer} />
+        <Switch>
+            <AuthRoute exact path="/login" component={LogInFormContainer} />
+            <AuthRoute excat path="/signup" component={SignUpFormContainer} />
+        </Switch>
     </div>
 );
 
