48ca3d2ab91b083fd97383fa7ad3b5a82211c2ad
spark <spark11491@gmail.com>
2021-11-02 15:50:57 -0400

    finish styling navbar login signup buttons

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,50 +3,26 @@ import { Link } from "react-router-dom";
 
 const Navbar = ({ currentUser, logout }) => {
     const userDisplay = currentUser ? (
-        <div className="navbar-items">
             <div className="navbar-dropdown">
                 <h2 className="user">Hello {currentUser.first_name}</h2>
                 <button className="navbar-content" onClick={logout}>Log Out</button>
             </div>
-        </div>
     ) : (
         <div className="login-signup">
             <div>
-                <Link className="login-button" to="/login">Login</Link>
-            </div>
-            <div>
-                <Link className="signup-button" to="/signup">Sign up</Link>
+                <Link className="login-button" to="/login">Log In</Link>
+                <Link className="signup-button" to="/signup">Sign Up</Link>
             </div>
         </div>
     )
 
     return (
-        <div className="right-navbar">
-            {userDisplay}
+        <div className="navbar">
+            <div className="right-navbar">
+                {userDisplay}
+            </div>
         </div>
     )
-
-    // const userGreeting = () => (
-    //     <div className="navbar-items">
-    //         <div className="navbar-dropdown">
-    //             <h2 className="user">{currentUser.first_name}</h2>
-    //             <button className="navbar-content" onClick={logout}>Log Out</button>
-    //         </div>
-    //     </div>
-    // );
-
-    // const sessionLinks = () => (
-    //     <div className="login-signup">
-    //         <div>
-    //             <Link to="/login">Login</Link>
-    //         </div>
-    //         <div>
-    //             <Link to="/signup">Sign up</Link>
-    //         </div>
-    //     </div>
-    // )
-
-    // return currentUser ? userGreeting() : sessionLinks();
 }
 
 export default Navbar;
