6ff00c8b3323da214d8e72b9a922d03d32482dc4
spark <spark11491@gmail.com>
2021-11-03 00:57:37 -0400

    fix businesses controller

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -1,9 +1,9 @@
-class Api::BusinessesController < ApplicationControlller
-    def index
-        @businesses = Business.all
-    end
-
-    def show
-        @business = Business.find_by(id: params[:id])
-    end
-end
+#class Api::BusinessesController < ApplicationControlller
+ #   def index
+  #      @businesses = Business.all
+   # end
+#
+ #   def show
+  #      @business = Business.find_by(id: params[:id])
+   # end
+#end
