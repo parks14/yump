4d2374070837eef82c4545e16dff7ea690a2f7c1
spark <spark11491@gmail.com>
2021-11-06 12:09:09 -0400

    fix business show image header

--- frontend/components/business/business_detail.jsx
+++ frontend/components/business/business_detail.jsx
@@ -3,32 +3,40 @@ import { withRouter } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 class BusinessDetail extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
+        const { business } = this.props;
+
         if (!this.props.business) {
             return null
         } else {
         return (
         <div className="business-page-container">
             <div className="business-header">
-                <div className="business-name">{this.props.business.name}</div>
+                <div className="business-img-container">
+                    {business.photoUrls.map((photo, i) => {
+                        return (
+                           <img className="business-img" 
+                                src={photo} 
+                                key={`img-${i}`}
+                                /> 
+                        )
+                    })}
+                </div>
+                <div className="business-name">{business.name}</div>
                 <div className="rating">Rating</div>
                 <div className="price-category-container">
-                    <div className="price">{this.props.business.price}</div>
-                    <div>{this.props.business.category}</div>
+                    <div className="price">{business.price}</div>
+                    <div>{business.category}</div>
                 </div>
-                <div className="hours">{this.props.business.hours}</div>
+                <div className="hours">{business.hours}</div>
             </div>
             <div className="right-navbar">
-                <a href={this.props.business.website} className="website"></a>
-                <div className="number">{this.props.business.phone_number}</div>
+                <a href={business.website} className="website"></a>
+                <div className="number">{business.phone_number}</div>
                 <div className="address">
-                    {this.props.business.address} 
-                    {this.props.business.city}, {this.props.business.state} 
-                    {this.props.business.zip_code}
+                    {business.address} 
+                    {business.city}, {business.state} 
+                    {business.zip_code}
                 </div>
             </div>
             <div className="details-container">
@@ -39,25 +47,25 @@ class BusinessDetail extends React.Component {
                     <div className="location">
                         <div className="map-snippet"></div>
                         <div className="address">
-                            <div className="street-address">{this.props.business.address}</div>
+                            <div className="street-address">{business.address}</div>
                             <div className="city-state">
-                                {this.props.business.city}, {this.props.business.state} {this.props.business.zip_code}
+                                {business.city}, {business.state} {business.zip_code}
                             </div>
                         </div>
                     </div>
                     <div className="hours-container">
-                        <div>Mon {this.props.business.hours}</div>
-                        <div>Tue {this.props.business.hours}</div>
-                        <div>Wed {this.props.business.hours}</div>
-                        <div>Thu {this.props.business.hours}</div>
-                        <div>Fri {this.props.business.hours}</div>
-                        <div>Sat {this.props.business.hours}</div>
-                        <div>Sun {this.props.business.hours}</div>
+                        <div>Mon {business.hours}</div>
+                        <div>Tue {business.hours}</div>
+                        <div>Wed {business.hours}</div>
+                        <div>Thu {business.hours}</div>
+                        <div>Fri {business.hours}</div>
+                        <div>Sat {business.hours}</div>
+                        <div>Sun {business.hours}</div>
                     </div>
                 </div>
                 <div className="reviews-container">Recommended Reviews
                     <div className="write-review-container">
-                        <p>Start your review of {this.props.business.name}.</p>
+                        <p>Start your review of {business.name}.</p>
                     </div>
                     <div className="reviews-container">
                         <p>Reviews</p>
